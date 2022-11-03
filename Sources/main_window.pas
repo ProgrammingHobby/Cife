af2de7d9607a80b7cc230d4900e98056c693513c
ProgrammingHobby <Uwe.Merker@KabelBW.de>
2022-11-03 16:33:34 +0100

    Menüstruktur und Statusleiste angelegt.

--- Sources/main_window.pas
+++ Sources/main_window.pas
@@ -5,10 +5,43 @@ unit main_window;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Menus, ComCtrls;
 
 type
+
+  { TMainWindow }
+
   TMainWindow = class(TForm)
+    MainMenu: TMainMenu;
+    menuFile: TMenuItem;
+    menuItemCopy: TMenuItem;
+    menuItemPaste: TMenuItem;
+    menuItemSelectAll: TMenuItem;
+    Separator3: TMenuItem;
+    menuItemRename: TMenuItem;
+    menuItemDelete: TMenuItem;
+    Separator4: TMenuItem;
+    menuItemAttributes: TMenuItem;
+    menuItemProtections: TMenuItem;
+    Separator5: TMenuItem;
+    menuItemNew: TMenuItem;
+    menuItemCreateNew: TMenuItem;
+    menuView: TMenuItem;
+    menuItemRefresh: TMenuItem;
+    menuTools: TMenuItem;
+    menuItemCheckImage: TMenuItem;
+    menuOptions: TMenuItem;
+    menuCopySettings: TMenuItem;
+    menuHelp: TMenuItem;
+    menuItemAbout: TMenuItem;
+    menuItemOpen: TMenuItem;
+    Separator1: TMenuItem;
+    menuItemClose: TMenuItem;
+    Separator2: TMenuItem;
+    menuItemQuit: TMenuItem;
+    menuEdit: TMenuItem;
+    menuItemCut: TMenuItem;
+    statusbarMainWindow: TStatusBar;
   private
 
   public
