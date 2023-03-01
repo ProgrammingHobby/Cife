2560131e37122a9c64f2a40840fb086f13484f02
ProgrammingHobby <Uwe.Merker@KabelBW.de>
2023-03-01 19:32:30 +0100

    'FileDialog' mit Grundfunktionalität erweitert.

--- Sources/file_dialog.pas
+++ Sources/file_dialog.pas
@@ -57,11 +57,24 @@ type
         ShellListView: TShellListView;
         ShellTreeView: TShellTreeView;
         Splitter: TSplitter;
+        procedure buttonDetailViewClick(Sender: TObject);
+        procedure buttonListViewClick(Sender: TObject);
+        procedure comboboxFileTypesChange(Sender: TObject);
         procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
+        procedure FormShow(Sender: TObject);
         procedure Panel4Paint(Sender: TObject);
+        procedure ShellListViewSelectItem(Sender: TObject; Item: TListItem; Selected: boolean);
     private
+        WildcardsList: TStringArray;
 
     public
+        procedure SetDialogTitle(title: string);
+        procedure SetRootPath(path: string);
+        procedure SetDefaultPath(path: string);
+        procedure SetDefaultFile(filename: string);
+        procedure SetFileWildcards(wildcards: string);
+        function GetFullFileName: string;
+        function GetImageType: string;
 
     end;
 
@@ -73,16 +86,70 @@ implementation
 {$R *.lfm}
 
 { TFileDialog }
+
+uses XMLSettings, CifeGlobals;
+
 // --------------------------------------------------------------------------------
 procedure TFileDialog.FormClose(Sender: TObject; var CloseAction: TCloseAction);
 begin
+    with TXMLSettings.Create(SettingsFile) do begin
+        try
+            SaveFormState(TForm(self));
+        finally
+            Free;
+        end;
+    end;
     CloseAction := caFree;
+end;
+
+// --------------------------------------------------------------------------------
+procedure TFileDialog.comboboxFileTypesChange(Sender: TObject);
+var
+    cmb: TComboBox;
+begin
+    if (Sender is TComboBox) then begin
+        cmb := TComboBox(Sender);
+        ShellListView.Mask := WildcardsList[(cmb.ItemIndex * 2) + 1];
+        ShellListView.Refresh;
+    end;
+end;
+
+// --------------------------------------------------------------------------------
+procedure TFileDialog.buttonListViewClick(Sender: TObject);
+begin
+    ShellListView.ViewStyle := vsIcon;
+    ShellListView.ShowColumnHeaders := False;
+    ShellListView.Repaint;
+end;
+
+// --------------------------------------------------------------------------------
+procedure TFileDialog.buttonDetailViewClick(Sender: TObject);
+begin
+    ShellListView.ViewStyle := vsReport;
+    ShellListView.ShowColumnHeaders := True;
+    ShellListView.Repaint;
+end;
+
+// --------------------------------------------------------------------------------
+procedure TFileDialog.FormShow(Sender: TObject);
+begin
+    GetDiskDefsList(comboboxImageTypes.Items);
+    SetAutoSize(False);
+    Constraints.MinWidth := Width;
+    Constraints.MinHeight := Height;
+    with TXMLSettings.Create(SettingsFile) do begin
+        try
+            RestoreFormState(TForm(self));
+        finally
+            Free;
+        end;
+    end;
 end;
 
 // --------------------------------------------------------------------------------
 procedure TFileDialog.Panel4Paint(Sender: TObject);
 const
-    R = 4;  // Rundungsradius
+    Radius = 4;
 var
     panel: TPanel;
 begin
@@ -93,7 +160,80 @@ begin
     panel.Canvas.Brush.Style := bsClear;
     panel.Canvas.Pen.Color := clSilver;
     panel.Canvas.Pen.Width := 1;
-    panel.Canvas.RoundRect(0, 0, panel.ClientWidth, panel.ClientHeight, R, R);
+    panel.Canvas.RoundRect(0, 0, panel.ClientWidth, panel.ClientHeight, Radius, Radius);
 end;
 
+// --------------------------------------------------------------------------------
+procedure TFileDialog.ShellListViewSelectItem(Sender: TObject; Item: TListItem; Selected: boolean);
+var
+    Slv: TShellListView;
+begin
+    if (Sender is TShellListView) then begin
+        Slv := TShellListView(Sender);
+        if Assigned(Slv.Selected) then begin
+            editFileName.Caption := Slv.Selected.Caption;
+        end;
+    end;
+end;
+
+// --------------------------------------------------------------------------------
+procedure TFileDialog.SetDialogTitle(title: string);
+begin
+    Caption := title;
+end;
+
+// --------------------------------------------------------------------------------
+procedure TFileDialog.SetRootPath(path: string);
+begin
+    ShellTreeView.Root := path;
+    labelFilePath.Caption := path;
+end;
+
+// --------------------------------------------------------------------------------
+procedure TFileDialog.SetDefaultPath(path: string);
+begin
+    ShellTreeView.Path := path;
+end;
+
+// --------------------------------------------------------------------------------
+procedure TFileDialog.SetDefaultFile(filename: string);
+begin
+    editFileName.Text := filename;
+end;
+
+// --------------------------------------------------------------------------------
+procedure TFileDialog.SetFileWildcards(wildcards: string);
+var
+    idx: integer;
+begin
+    comboboxFileTypes.Clear;
+    WildcardsList := wildcards.Split('|');
+    idx := 0;
+    while (idx < Length(WildcardsList)) do begin
+        comboboxFileTypes.Items.Add(WildcardsList[idx]);
+        Inc(idx, 2);
+    end;
+    comboboxFileTypes.ItemIndex := 0;
+    comboboxFileTypesChange(comboboxFileTypes);
+end;
+
+// --------------------------------------------------------------------------------
+function TFileDialog.GetFullFileName: string;
+begin
+    Result := '';
+    if Assigned(ShellListView.Selected) then begin
+        Result := IncludeTrailingPathDelimiter(ShellListView.Root) + ShellListView.Selected.Caption;
+    end;
+end;
+
+// --------------------------------------------------------------------------------
+function TFileDialog.GetImageType: string;
+begin
+    Result := '';
+    if (comboboxImageTypes.ItemIndex > -1) then begin
+        Result := comboboxImageTypes.Items[comboboxImageTypes.ItemIndex];
+    end;
+end;
+
+// --------------------------------------------------------------------------------
 end.
