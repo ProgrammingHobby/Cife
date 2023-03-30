7cdba8e90c4e4362039bcc20c00bcf174de66c24
ProgrammingHobby <Uwe.Merker@KabelBW.de>
2023-03-30 18:49:09 +0200

    Klasse 'CpmDevice' zum sektorweisen Lesen und Schreiben eines Images fertiggestellt.

--- Sources/cpmdevice.pas
+++ Sources/cpmdevice.pas
@@ -29,10 +29,19 @@ type
 
     { TCpmDevice }
 
+    TDeviceMode = (dmOpenRead, dmOpenWrite, dmOpenReadWrite);
+
     TCpmDevice = class
     public    // Attribute
 
     public    // Methoden
+        function Open(const AFilename: string; const AMode: TDeviceMode = dmOpenRead): boolean;
+        procedure SetGeometry(const ASecLength, ASecTrk, ATracks, AOffset: integer);
+        function Close: boolean;
+        function ReadSector(const ATrack, ASector: integer; var ABuffer: array of byte): boolean;
+        function WriteSector(const ATrack, ASector: integer; const ABuffer: array of byte): boolean;
+        function IsOpen(): boolean;
+        function GetErrorMsg(): string;
 
     public  // Konstruktor/Destruktor
         constructor Create; overload;
@@ -43,6 +52,19 @@ type
     protected // Methoden
 
     private   // Attribute
+    type
+        TDevice = record
+            Opened: boolean;
+            SecLength: integer;
+            Tracks: integer;
+            SecTrk: integer;
+            Offset: integer;
+            Imagefile: file;
+        end;
+
+    var
+        FDevice: TDevice;
+        FDeviceError: string;
 
     private   // Methoden
 
@@ -51,6 +73,109 @@ type
 implementation
 
 { TCpmDevice }
+
+// --------------------------------------------------------------------------------
+function TCpmDevice.Open(const AFilename: string; const AMode: TDeviceMode): boolean;
+begin
+    case (AMode) of
+        dmOpenRead: FileMode := fmOpenRead;
+        dmOpenWrite: FileMode := fmOpenWrite;
+        dmOpenReadWrite: FileMode := fmOpenReadWrite;
+        else FileMode := fmOpenRead;
+    end;
+    FDevice.Opened := False;
+    try
+        AssignFile(FDevice.Imagefile, AFilename);
+        Reset(FDevice.Imagefile, 1);
+        FDevice.Opened := True;
+    except
+        on e: Exception do begin
+            FDeviceError := e.Message;
+        end;
+    end;
+    Result := FDevice.Opened;
+end;
+
+// --------------------------------------------------------------------------------
+procedure TCpmDevice.SetGeometry(const ASecLength, ASecTrk, ATracks, AOffset: integer);
+begin
+    FDeviceError := EmptyStr;
+    FDevice.SecLength := ASecLength;
+    FDevice.SecTrk := ASecTrk;
+    FDevice.Tracks := ATracks;
+    FDevice.Offset := AOffset;
+end;
+
+// --------------------------------------------------------------------------------
+function TCpmDevice.Close: boolean;
+begin
+    try
+        CloseFile(FDevice.Imagefile);
+        FDevice.Opened := False;
+    except
+        on e: Exception do begin
+            FDeviceError := e.Message;
+        end;
+    end;
+    Result := not FDevice.Opened;
+end;
+
+// --------------------------------------------------------------------------------
+function TCpmDevice.ReadSector(const ATrack, ASector: integer; var ABuffer: array of byte): boolean;
+var
+    Count: integer;
+begin
+    Count := 0;
+    FDeviceError := EmptyStr;
+    try
+        Reset(FDevice.Imagefile, 1);
+        Seek(FDevice.Imagefile, (((ASector + (ATrack * FDevice.sectrk)) * FDevice.SecLength) + FDevice.Offset));
+        BlockRead(FDevice.Imagefile, ABuffer, FDevice.SecLength, Count);
+        Result := True;
+    except
+        on e: Exception do begin
+            if (Count <> FDevice.SecLength) then begin
+                FDeviceError := Format('Read Sector: %d bytes lost. Error: %s', [(FDevice.SecLength - Count), e.Message]);
+            end;
+            Result := False;
+        end;
+    end;
+end;
+
+// --------------------------------------------------------------------------------
+function TCpmDevice.WriteSector(const ATrack, ASector: integer; const ABuffer: array of byte): boolean;
+var
+    Count: integer;
+begin
+    Count := 0;
+    FDeviceError := EmptyStr;
+    try
+        Reset(FDevice.Imagefile, 1);
+        Seek(FDevice.Imagefile, (((ASector + (ATrack * FDevice.SecTrk)) * FDevice.SecLength) + FDevice.Offset));
+        BlockWrite(FDevice.Imagefile, ABuffer, FDevice.SecLength, Count);
+        Result := True;
+    except
+        on e: Exception do begin
+            if (Count <> FDevice.SecLength) then begin
+                FDeviceError := Format('Write Sector: %d bytes lost. Error: %s', [(FDevice.SecLength - Count), e.Message]);
+            end;
+            Result := False;
+        end;
+    end;
+end;
+
+// --------------------------------------------------------------------------------
+function TCpmDevice.IsOpen: boolean;
+begin
+    Result := FDevice.Opened;
+end;
+
+// --------------------------------------------------------------------------------
+function TCpmDevice.GetErrorMsg: string;
+begin
+    Result := FDeviceError;
+    FDeviceError := EmptyStr;
+end;
 
 // --------------------------------------------------------------------------------
 constructor TCpmDevice.Create;
