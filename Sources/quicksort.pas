44e8085d8f4df521844554cea921ad860a06f516
ProgrammingHobby <Uwe.Merker@KabelBW.de>
2023-05-08 17:56:22 +0200

    Unit 'QuickSort' angelegt.

---
+++ Sources/quicksort.pas
@@ -0,0 +1,124 @@
+{***************************************************************************
+ *                                                                         *
+ *   This source is free software; you can redistribute it and/or modify   *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This code is distributed in the hope that it will be useful, but      *
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *   General Public License for more details.                              *
+ *                                                                         *
+ *   A copy of the GNU General Public License is available on the World    *
+ *   Wide Web at <http://www.gnu.org/copyleft/gpl.html>. You can also      *
+ *   obtain it by writing to the Free Software Foundation,                 *
+ *   Inc., 51 Franklin Street - Fifth Floor, Boston, MA 02110-1335, USA.   *
+ *                                                                         *
+ ***************************************************************************}
+unit QuickSort;
+
+{$mode ObjFPC}{$H+}
+
+interface
+
+uses
+    Classes, SysUtils;
+
+procedure QSort(var arr: TStringList; Start: longint; Finish: longint; SortUp: boolean = True);
+
+implementation
+
+procedure QSort(var arr: TStringList; Start: longint; Finish: longint; SortUp: boolean = True);
+var
+    i, j: longint;
+    x, temp: ansistring;
+    u: array[0..255] of byte;  {look up table for speed}
+
+    function lessthan(const a: ansistring; const b: ansistring): integer;
+    var
+        n, lim, lena, lenb: longint;
+    begin
+        lena := length(a);
+        lenb := length(b);
+        if (lena < lenb) then
+            lim := lena
+        else
+            lim := lenb;
+        for n := 1 to lim do begin
+            if u[Ord(a[n])] < u[Ord(b[n])] then
+                exit(-1);
+            if u[Ord(a[n])] > u[Ord(b[n])] then
+                exit(0);
+        end;
+        exit(0);
+    end;
+
+    function morethan(const a: ansistring; const b: ansistring): integer;
+    var
+        n, lim, lena, lenb: longint;
+    begin
+        lena := length(a);
+        lenb := length(b);
+        if (lena < lenb) then
+            lim := lena
+        else
+            lim := lenb;
+        for n := 1 to lim do begin
+            if u[Ord(a[n])] > u[Ord(b[n])] then
+                exit(-1);
+            if u[Ord(a[n])] < u[Ord(b[n])] then
+                exit(0);
+        end;
+        exit(0);
+    end;
+
+    procedure setlookuparray(var u: array of byte); // must run
+    var
+        x, r: integer;
+    begin
+        for x := 0 to 255 do begin
+            r := x;
+   {$ifdef caseinsensitive}
+            if (r < 91) and (r > 64) then
+                r := r + 32;
+   {$endif}
+            u[x] := r;
+        end;
+    end;
+
+begin
+    i := Start;
+    j := finish;
+    x := arr[(Start + finish) div 2];
+    setlookuparray(u);
+    while I <= J do begin
+        if SortUp then begin
+            while lessthan(arr[I], X) = -1 do
+                I += 1;
+            while morethan(arr[J], X) = -1 do
+                J -= 1;
+        end;
+
+        if not SortUp then begin
+            while morethan(arr[I], X) = -1 do
+                I += 1;
+            while lessthan(arr[J], X) = -1 do
+                J -= 1;
+        end;
+
+        if I <= J then begin
+            temp := arr[j];
+            arr[j] := arr[i];
+            arr[i] := temp;
+            I += 1;
+            J -= 1;
+        end;
+    end;
+    if J > Start then
+        QSort(arr, Start, J, SortUp);
+    if I < Finish then
+        QSort(arr, I, Finish, SortUp);
+end;
+
+end.
